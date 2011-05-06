18afc85bd883d6096923c3b505132912166abb26
Moritz Krog <moritzkrog@projectscio.org>
2011-05-06 17:20:08 +0200

    The aggregate_feeds template_tag now can parse integer parameters, as well as the keyword 'all'

--- aggregator/templatetags/aggregates.py
+++ aggregator/templatetags/aggregates.py
@@ -1,10 +1,19 @@
+try: from settings import aggregate_feeds_template
+except ImportError: from aggregator.aggregator_settings import aggregate_feeds_template
+
 from django import template
-from aggregator.models import Entry
+from django.utils.safestring import SafeUnicode
+from aggregator.models import Feed, Entry
 
 register = template.Library()
 
-@register.inclusion_tag("templatetags/aggregate_feeds.html")
+@register.inclusion_tag(aggregate_feeds_template)
 def aggregate_feeds(amount):
+	if type(amount) == int:
+		entries = Entry.objects.all()[:int(amount)]
+	elif isinstance(amount, SafeUnicode) and amount == 'all':
+		entries = Entry.objects.all()
+	else:
+		raise Exception()
 
-	entries = Entry.objects.all()[:int(amount)]
 	return {'entries': entries}
