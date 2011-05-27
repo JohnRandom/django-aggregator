8e254ccb6aa25b50860701104278099c0fff2418
Moritz Krog <moritzkrog@gmail.com>
2011-05-27 20:04:00 +0200

    Removed old aggregate_feeds template tag in favor of the new one Removed features for templatetags

--- aggregator/templatetags/aggregates.py
+++ aggregator/templatetags/aggregates.py
@@ -7,17 +7,6 @@ from aggregator.models import Feed, Entry, StaticContent
 
 register = template.Library()
 
-@register.inclusion_tag(aggregate_feeds_template)
-def aggregate_feeds(amount):
-	if type(amount) == int:
-		entries = Entry.objects.all()[:int(amount)]
-	elif (isinstance(amount, SafeUnicode) or type(amount) == unicode) and amount == 'all':
-		entries = Entry.objects.all()
-	else:
-		raise Exception("'amount' must be an instance of SafeUnicode, found %s instead" % amount.__class__.__name__)
-
-	return {'entries': entries}
-
 @register.simple_tag
 def static_content(identifier):
 	try: content = StaticContent.objects.get(name = identifier)
