d7a6fdcd5895c7c286dc81478418afe39a9d837d
Moritz Krog <moritzkrog@gmail.com>
2011-06-01 00:11:58 +0200

    Resolved a bug where 'update_all link was reffering' to the wrong next url

--- aggregator/urls.py
+++ aggregator/urls.py
@@ -4,5 +4,5 @@ from aggregator.models import StaticContent, Feed
 
 urlpatterns = patterns('aggregator.views',
     url(r'^playground/$', PlaygroundView.as_view(), name = 'playground_view'),
-	url(r'^update/$', 'update_content', {'model': [StaticContent, Feed], 'next': '/playground/'}, name = 'update_view'),
+	url(r'^update/$', 'update_content', {'model': [StaticContent, Feed]}, name = 'update_view'),
 )
