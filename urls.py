43d7be3e4ee0d638dbb000132af3c5c59454b4d0
Moritz Krog <moritzkrog@gmail.com>
2011-05-31 23:42:02 +0200

    Added the landing_page app in which content configuration will be done Added migrations for landing_page Added an index view to map on '/'

--- urls.py
+++ urls.py
@@ -3,9 +3,12 @@ from django.core.urlresolvers import reverse
 from django.contrib import admin
 admin.autodiscover()
 
+from landing_page.views import IndexView
+
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'aggregator/', include('aggregator.urls')),
+	url(r'^$', IndexView.as_view(), name = 'index_view'),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'aggregator/', include('aggregator.urls')),
 	url(r'^accounts/login/$', 'django.contrib.auth.views.login', name = 'login_view'),
 	url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name = 'logout_view'),
 )
