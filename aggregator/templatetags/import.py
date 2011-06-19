61276a501c850166eba72d94d71f983369af8c98
Moritz Krog <moritzkrog@gmail.com>
2011-06-19 21:45:09 +0200

    Fixed a bug in css-import where static url was used instead of static root

--- aggregator/templatetags/import.py
+++ aggregator/templatetags/import.py
@@ -5,22 +5,22 @@ from django import template
 from django.conf import settings
 
 register = template.Library()
-STATIC_URL = settings.STATIC_URL
-CSS_ROOT = os.path.join(settings.STATIC_URL, 'css/')
+STATIC_ROOT = settings.STATIC_ROOT
+CSS_ROOT = os.path.join(settings.STATIC_ROOT, 'css/')
 LINK_TAG = '<link href="%s" rel="stylesheet" type="text/css">'
 
 @register.simple_tag
 def all_stylesheets():
-	return stylesheets('.')
+    return stylesheets('.')
 
 @register.simple_tag
 def stylesheets(directory):
-	links = []
+    links = []
 
-	root = os.path.join(CSS_ROOT, os.path.normpath(directory), '*.css')
-	for css_file in glob(root):
-		if not os.path.isfile(css_file): continue
-		ressource_url = settings.MEDIA_URL + os.path.relpath(css_file, STATIC_URL)
-		links.append(LINK_TAG % ressource_url)
+    root = os.path.join(CSS_ROOT, os.path.normpath(directory), '*.css')
+    for css_file in glob(root):
+        if not os.path.isfile(css_file): continue
+        ressource_url = settings.MEDIA_URL + os.path.relpath(css_file, STATIC_ROOT)
+        links.append(LINK_TAG % ressource_url)
 
-	return '\n'.join(links)
+    return '\n'.join(links)
